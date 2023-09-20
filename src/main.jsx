b5020b13d05d0c76fbb6ba97aeb860338aef8cb9
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2023-09-19 21:41:52 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
   {path:'/',
   element: <App/>,
   children: [
-    {path: '/', element: <Home/>},
+    {path: '/Get_a_pet', element: <Home/>},
     {path: '/register', element: <Register/>},
     {path: '/login', element: <Login/>},
     {path: '/users/profile', element: <Profile/>},
