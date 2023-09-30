55266a6cea8cdead7abe90af4e86cfcf9b6d0d52
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2023-09-30 12:31:38 -0300

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,18 +19,18 @@ const Navbar = () => {
                 <h2>Get a pet</h2>
             </div>
             <ul>
-                <li><Link to='/Get_a_pet'>Adotar</Link></li>
+                <li><Link to='/'>Adotar</Link></li>
                 {authenticaded ? (
-                <>  <li><Link to={`${import.meta.env.DEPLOY_URL}/pets/myadoptions`}>Minhas adoções</Link></li>
-                    <li><Link to={`${import.meta.env.DEPLOY_URL}/pets/mypets`}>Meus pets</Link></li>
-                    <li><Link to={`${import.meta.env.DEPLOY_URL}/users/profile`}>Perfil</Link></li>
+                <>  <li><Link to={`/pets/myadoptions`}>Minhas adoções</Link></li>
+                    <li><Link to={`/pets/mypets`}>Meus pets</Link></li>
+                    <li><Link to={`/users/profile`}>Perfil</Link></li>
                     <li onClick={logout}>Sair</li>
                     
                 </>): 
 
                 (<>
-                    <li><Link to={`${import.meta.env.DEPLOY_URL}/login`}>Entrar</Link></li>
-                    <li><Link to={`${import.meta.env.DEPLOY_URL}/register`}>Registrar</Link></li>
+                    <li><Link to={`/login`}>Entrar</Link></li>
+                    <li><Link to={`/register`}>Registrar</Link></li>
                 </>
                 )}
                 
