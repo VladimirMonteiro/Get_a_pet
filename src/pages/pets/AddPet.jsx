264e52b07591b3536e6a180fd6b2903501bb53d2
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2023-09-30 23:57:44 -0300

    commited

--- src/pages/pets/AddPet.jsx
+++ src/pages/pets/AddPet.jsx
@@ -9,57 +9,57 @@ import PetForm from '../../components/form/PetForm'
 
 
 function AddPet() {
-    const [token] = useState(localStorage.getItem('token') || '')
-    const { setFlashMessage } = useFlashMessage()
-    const navigate = useNavigate()
-  
-    async function registerPet(pet) {
-      let msgType = 'sucess'
-  
-      const formData = new FormData()
-  
-      const petFormData = await Object.keys(pet).forEach((key) => {
-        if (key === 'images') {
-          for (let i = 0; i < pet[key].length; i++) {
-            formData.append(`images`, pet[key][i])
-          }
-        } else {
-          formData.append(key, pet[key])
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { setFlashMessage } = useFlashMessage()
+  const navigate = useNavigate()
+
+  async function registerPet(pet) {
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    const petFormData = await Object.keys(pet).forEach((key) => {
+      if (key === 'images') {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append(`images`, pet[key][i])
         }
+      } else {
+        formData.append(key, pet[key])
+      }
+    })
+
+    formData.append('pet', petFormData)
+
+    const data = await api
+      .post(`pets/create`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          'Content-Type': 'multipart/form-data',
+        },
       })
-  
-      formData.append('pet', petFormData)
-  
-      const data = await api
-        .post(`pets/create`, formData, {
-          headers: {
-            Authorization: `Bearer ${JSON.parse(token)}`,
-            'Content-Type': 'multipart/form-data',
-          },
-        })
-        .then((response) => {
-          console.log(response.data)
-          return response.data.newPet
-        })
-        .catch((err) => {
-          console.log(err)
-          msgType = 'error'
-          return err.response.data
-        })
-  
-      setFlashMessage(data.message, msgType)
-      navigate('/pets/mypets')
-    }
-  
-    return (
-      <section>
-        <div className={styles.addpet_header}>
-          <h1>Cadastre um Pet</h1>
-          <p>Depois ele ficará disponível para adoção</p>
-        </div>
-        <PetForm handleSubmit={registerPet} btnText="Cadastrar" />
-      </section>
-    )
+      .then((response) => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+    navigate('/pet/mypets')
   }
-  
-  export default AddPet
+
+  return (
+    <section>
+      <div className={styles.addpet_header}>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficará disponível para adoção</p>
+      </div>
+      <PetForm handleSubmit={registerPet} btnText="Cadastrar" />
+    </section>
+  )
+}
+
+export default AddPet
